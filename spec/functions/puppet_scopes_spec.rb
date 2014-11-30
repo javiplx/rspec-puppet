74bfacb6da0e4214fb9a41ecffec5991a752c879
Javier Palacios <javiplx@gmail.com>
2014-11-30 15:00:00 +0100

    Add tests for parent_module_name

--- spec/functions/puppet_scopes_spec.rb
+++ spec/functions/puppet_scopes_spec.rb
@@ -19,3 +19,27 @@ describe 'get_resource' do
 
 end
 
+describe 'get_module_name' do
+
+  context 'called on top scope' do
+    let(:node) { '' }
+    it { should run.with_params('any').and_return('') }
+  end
+
+  context 'called on node' do
+    let(:node) { 'testhost_a' }
+    it { should run.with_params('any').and_return('') }
+  end
+
+  context 'called on class' do
+    let(:class_scope) { 'rspec' }
+    it { should run.with_params('any').and_return('rspec') }
+  end
+
+  context 'called on class' do
+    let(:class_scope) { 'rspec::subclass' }
+    it { should run.with_params('any').and_return('rspec') }
+  end
+
+end
+
