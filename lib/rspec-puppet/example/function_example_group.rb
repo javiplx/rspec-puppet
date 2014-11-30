4442a3af51a2cc2e0794723a4654c6cbb3efd867
Javier Palacios <javiplx@gmail.com>
2014-11-30 14:53:34 +0100

    Allow specification of a class to run functions on

--- lib/rspec-puppet/example/function_example_group.rb
+++ lib/rspec-puppet/example/function_example_group.rb
@@ -12,7 +12,9 @@ module RSpec::Puppet
       node_name = nodename(:function)
 
       function_scope = scope(compiler, node_name)
-      if node_name.empty?
+      if self.respond_to?(:class_scope) && !class_scope.nil?
+        main_resource = Puppet::Parser::Resource.new('class', class_scope , { :scope => function_scope })
+      elsif node_name.empty?
         main_resource = Puppet::Parser::Resource.new('class', 'main' , { :scope => function_scope })
       else
         main_resource = Puppet::Parser::Resource.new('node', node_name , { :scope => function_scope })
