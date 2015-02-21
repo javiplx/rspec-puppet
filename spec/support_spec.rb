9fd4fa8a738e7a215a79ace1494bd42cdf7992d0
Javier Palacios <javiplx@gmail.com>
2015-02-21 21:14:07 +0100

    Add tests for module_repository configuration

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -18,5 +18,14 @@ describe RSpec::Puppet::Support do
       subject.setup_puppet
       expect(Puppet[:parser]).to eq("future")
     end
+    it 'sets Puppet[:module_repository] to puppetlabs forge by default' do
+      subject.setup_puppet
+      expect(Puppet[:module_repository]).to eq("https://forgeapi.puppetlabs.com")
+    end
+    it 'reads the :module_repository setting' do
+      allow(subject).to receive(:module_repository).and_return("http://puppet-library.example.com")
+      subject.setup_puppet
+      expect(Puppet[:module_repository]).to eq("http://puppet-library.example.com")
+    end
   end
 end
